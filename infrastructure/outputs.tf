e1237af77963dc60cca8d857c9a6841b329abadf
marcinmolenda <m.molenda@makeit24.pl>
2022-08-23 22:17:27 +0200

    output changes

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,5 +1,6 @@
 output "app_service_plan" {
   value = {
+    id = azurerm_app_service_plan.this.id
     name = azurerm_app_service_plan.this.name
     resource_group_name = azurerm_resource_group.this.name
   }
