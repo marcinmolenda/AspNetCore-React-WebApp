e636f701a5fc10f8fce84c2414f99d4201de8ffe
marcinmolenda <m.molenda@makeit24.pl>
2022-08-24 21:14:48 +0200

    sql database 3

--- service/Microsoft.DSX.ProjectTemplate.Data/ProjectTemplateDbContextFactory.cs
+++ service/Microsoft.DSX.ProjectTemplate.Data/ProjectTemplateDbContextFactory.cs
@@ -8,7 +8,7 @@ namespace Microsoft.DSX.ProjectTemplate.Data
         public ProjectTemplateDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ProjectTemplateDbContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDb;Database=ProjectTemplate;Trusted_Connection=True;MultipleActiveResultSets=True;");
+            optionsBuilder.UseSqlServer("Server=tcp:demo-sql-weu-backend.database.windows.net,1433;Initial Catalog=sqldatabasebackend1;Persist Security Info=False;User ID=4dm1n157r470r;Password=v-:a%pbjPQx#)<w};MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             return new ProjectTemplateDbContext(optionsBuilder.Options);
         }
     }
