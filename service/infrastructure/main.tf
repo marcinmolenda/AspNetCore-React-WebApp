2def0210f6fd3aeda75ea5d19944af039a6bdabf
marcinmolenda <m.molenda@makeit24.pl>
2022-08-24 20:42:39 +0200

    sql database

--- service/infrastructure/main.tf
+++ service/infrastructure/main.tf
@@ -11,7 +11,12 @@ terraform {
         source = "aztfmod/azurecaf"
         version = "1.2.6"
     }
+
+    random = {
+        source = "hashicorp/random"
+        version = "3.3.2"
     }
+  }
 }
 
     provider "azurerm" {
@@ -25,7 +30,9 @@ terraform {
 resource "azurecaf_name" "this" {
     resource_types = [ 
         "azurerm_resource_group",
-        "azurerm_app_service" 
+        "azurerm_app_service",
+        "azurerm_sql_server",
+        "azurerm_sql_database"
     ]
     name = "weu"
     prefixes = [ "demo" ]
@@ -44,5 +51,41 @@ resource "azurerm_app_service" "this" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   app_service_plan_id = data.terraform_remote_state.infrastructure.outputs.app_service_plan.id
+  connection_string {
+    name  = "ConnectionStrings__Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.this.name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.this.name};Persist Security Info=False;User ID=${azurerm_sql_server.this.administrator_login};Password=${random_password.this.result};MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+  site_config {
+    dotnet_framework_version = "v6.0"
+  }
+}
 
+resource "random_password" "this" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "azurerm_sql_server" "this" {
+  name                         = azurecaf_name.this.results["azurerm_sql_server"]
+  resource_group_name          = azurerm_resource_group.this.name
+  location                     = azurerm_resource_group.this.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = random_password.this.result
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_sql_database" "this" {
+  name                = azurecaf_name.this.results["azurerm_sql_database"]
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  server_name         = azurerm_sql_server.this.name
+  tags = {
+    environment = "production"
+  }
 }
